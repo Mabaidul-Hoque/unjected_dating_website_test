77a453a746533cd2049136f65376314b63e01d24
MABAIDUL HOQUE <132288979+Mabaidul-Hoque@users.noreply.github.com>
2024-09-16 19:14:22 +0530

    Add files via upload

---
+++ src/app/page.js
@@ -0,0 +1,32 @@
+import { Appdownload, Connection, FAQ, Footer, Header, HeroImageSection, Ourmission, Realprofile, ResponsiveLayout, Speeddating, SuccesStories } from "@/Components";
+
+export default function Home() {
+  
+  return (
+    <div className="min-h-screen min-w-screen flex justify-start items-center flex-col">
+      <Header />
+
+      <HeroImageSection />
+
+      <Ourmission />
+
+      <Realprofile />
+
+      <Speeddating />
+
+      <ResponsiveLayout />
+
+      <SuccesStories />
+
+      <FAQ />
+    
+      <Appdownload />
+
+      <Connection />
+
+      <Footer />
+      
+    </div>
+ 
+  );
+}
