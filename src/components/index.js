10f13b1564fda61dd7cd08e7c685f4649cece576
MABAIDUL HOQUE <132288979+Mabaidul-Hoque@users.noreply.github.com>
2024-09-16 19:21:53 +0530

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -2,7 +2,7 @@ export { default as  Header } from './Header/Header';
 export { default as  HeroImageSection } from './hero-image-section/HeroImageSection';
 export { default as  Ourmission } from './Body/ourmission';
 export { default as  Realprofile } from './Body/Realprofile';
-export { default as  Speeddating } from './Body/Speeddating';
+export { default as  Speeddating } from './Body/speeddating';
 export { default as  ResponsiveLayout } from './bento-grid/ResponsiveLayout';
 export { default as  SuccesStories } from './success-stories/SuccesStories';
 export { default as  FAQ } from './faq/FAQ';
